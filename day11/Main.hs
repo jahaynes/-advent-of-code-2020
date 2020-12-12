b12d3309e2fe349c41d1ae003186ff869bf34061
John Haynes <john.andrew.haynes@gmail.com>
2020-12-12 23:06:33 +0100

    performance day 11

--- day11/Main.hs
+++ day11/Main.hs
@@ -1,10 +1,16 @@
-import           Data.Vector      (Vector, (!))
-import qualified Data.Vector as V
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString as BS
+import           Data.ByteString             (ByteString)
+import qualified Data.ByteString.Char8 as C8
+import           Data.Vector.Unboxed         ((!))
+import qualified Data.Vector.Unboxed as VU
+import           Data.Word                   (Word8)
 
 data Coord =
     Coord !Int !Int deriving Show
 
-_sampleInput1 :: String
+_sampleInput1 :: ByteString
 _sampleInput1 = "L.LL.LL.LL\n\
                 \LLLLLLL.LL\n\
                 \L.L.L..L..\n\
@@ -16,7 +22,7 @@ _sampleInput1 = "L.LL.LL.LL\n\
                 \L.LLLLLL.L\n\
                 \L.LLLLL.LL\n"
 
-_sampleInput2 :: String
+_sampleInput2 :: ByteString
 _sampleInput2 = ".......#.\n\
                 \...#.....\n\
                 \.#.......\n\
@@ -27,28 +33,27 @@ _sampleInput2 = ".......#.\n\
                 \#........\n\
                 \...#.....\n"
 
-data Grid a =
+data Grid =
     Grid { getHeight :: !Int
          , getWidth  :: !Int
-         , getCells  :: !(Vector (Vector a))
+         , getCells  :: !(VU.Vector Word8)
          } deriving Eq
 
-update :: (Coord -> a -> a) -> Grid a -> Grid a
+update :: (Coord -> Word8 -> Word8) -> Grid -> Grid
 update f grid =
     grid { getCells =
-        V.imap (\x row ->
-            V.imap (\y cell ->
-                f (Coord x y) cell) row) (getCells grid) }
+        VU.imap (\i cell -> let (x, y) = i `divMod` getWidth grid
+                            in f (Coord x y) cell) (getCells grid) }
 
-count :: (a -> Bool) -> Grid a -> Int
-count f = V.sum
-        . V.map (V.length . V.filter id . V.map f)
-        . getCells
+countHashes :: Grid -> Int
+countHashes = VU.length 
+            . VU.filter (==hash)
+            . getCells
 
 plus :: Coord -> (Int, Int) -> Coord
 plus (Coord x y) (x', y') = Coord (x+x') (y+y')
 
-inBounds :: Grid a -> Coord -> Bool
+inBounds :: Grid -> Coord -> Bool
 inBounds grid (Coord x y) | x  <             0  = False
                           | x >= getHeight grid = False
                           | y  <             0  = False
@@ -60,29 +65,34 @@ directions = [ (i, j) | i <- [-1, 0, 1],
                         j <- [-1, 0, 1],
                         i /= 0 || j /= 0 ]
 
-lkup :: Grid a -> Coord -> a
-lkup grid (Coord x y) = getCells grid ! x ! y
+lkup :: Grid -> Coord -> Word8
+lkup grid (Coord x y) = getCells grid ! (x * getWidth grid + y)
 
-part1 :: Grid Char -> Int
+hash, el, dot :: Word8
+hash = 35
+el   = 76
+dot  = 46
+
+part1 :: Grid -> Int
 part1 grid =
     let grid' = update step grid
     in if grid' == grid
-          then count (=='#') grid'
+          then countHashes grid'
           else part1 grid'
 
     where
-    step :: Coord -> Char -> Char
+    step :: Coord -> Word8 -> Word8
     step coord cell
 
-        | cell == 'L' && occupiedNeighbours coord == 0 = '#'
+        | cell == el   && occupiedNeighbours coord == 0 = hash
 
-        | cell == '#' && occupiedNeighbours coord >= 4 = 'L'
+        | cell == hash && occupiedNeighbours coord >= 4 = el
 
         | otherwise = cell
 
     occupiedNeighbours :: Coord -> Int
     occupiedNeighbours = length
-                       . filter (=='#')
+                       . filter (==hash)
                        . map (lkup grid)
                        . neighbours
 
@@ -91,40 +101,41 @@ part1 grid =
                      . map (plus coord)
                      $ directions
 
-part2 :: Grid Char -> Int
+part2 :: Grid -> Int
 part2 grid =
     let grid' = update step grid
     in
     if grid' == grid
-        then count (=='#') grid'
+        then countHashes grid'
         else part2 grid'
 
     where
-    step :: Coord -> Char -> Char
+    step :: Coord -> Word8 -> Word8
     step coord cell
 
-        | cell == 'L' && (length . filter (=='#') $ visible coord) == 0 = '#'
+        | cell == el   && (length . filter (==hash) $ visible coord) == 0 = hash
 
-        | cell == '#' && (length . filter (=='#') $ visible coord) >= 5 = 'L'
+        | cell == hash && (length . filter (==hash) $ visible coord) >= 5 = el
 
         | otherwise = cell
 
-    visible :: Coord -> [Char]
+    visible :: Coord -> [Word8]
     visible coord = castTo coord <$> directions
         where
         castTo c dir =
             let c' = plus c dir
             in
             if inBounds grid c'
-                then case lkup grid c' of
-                        '.' -> castTo c' dir
-                        x   -> x
-                else '.'
+                then let x = lkup grid c'
+                     in if x == dot
+                            then castTo c' dir
+                            else x
+                else dot
 
 main :: IO ()
 main = do
 
-    input <- readFile "day11/input"
+    input <- C8.readFile "day11/input"
 
     let grid = parseGrid input
 
@@ -133,10 +144,13 @@ main = do
     print $ part2 grid
 
     where
-    parseGrid :: String -> Grid Char
+    parseGrid :: ByteString -> Grid
     parseGrid input =
-        let cells = V.fromList (V.fromList <$> lines input)
-        in Grid { getHeight = length cells
-                , getWidth  = length $ V.head cells
+        let len    = C8.length input
+            height = length $ C8.lines input
+            width  = len `div` height
+            cells  = VU.fromListN len $ BS.unpack input
+        in Grid { getHeight = height
+                , getWidth  = width
                 , getCells  = cells
                 }
